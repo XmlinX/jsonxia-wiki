ddd96d9c1fbdf6039d0c6836fdb392e4823e9015
XML <547816252@qq.com>
2023-01-16 21:30:27 +0800

    2-2 project init

---
+++ src/main/java/com/jsonxia/wiki/WikiApplication.java
@@ -0,0 +1,18 @@
+package com.jsonxia.wiki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author xiameilin
+ */
+@SpringBootApplication
+public class WikiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WikiApplication.class, args);
+	}
+
+}
+
+
