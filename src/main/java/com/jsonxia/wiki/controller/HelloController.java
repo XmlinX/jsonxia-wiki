8ca2bfb690f629cc01236824f03568a45e06632f
XML <547816252@qq.com>
2023-01-17 21:22:23 +0800

    2-8 自定义配置项目及设置默认值

--- src/main/java/com/jsonxia/wiki/controller/HelloController.java
+++ src/main/java/com/jsonxia/wiki/controller/HelloController.java
@@ -1,6 +1,7 @@
 package com.jsonxia.wiki.controller;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,10 +16,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+    @Value("${test.hello:lao-ba}")
+    private String str;
+
 
 //    @GetMapping("/hello")
     @RequestMapping(value="/hello",method = RequestMethod.GET)
     public String hello(){
         return "hello world !";
     }
+
+    @RequestMapping(value="/v2/hello",method = RequestMethod.GET)
+    public String helloV2(){
+        return str;
+    }
+
+
 }
